1f434d5aa421fcbdf24e4bd6a6b6ceafc0999b96
root <root@srv1698532.hstgr.cloud>
2026-06-01 14:54:26 +0000

    fix: use okio.ByteString for WebSocket.send()

--- android/app/src/main/java/com/krishna/meetingassistant/WebSocketClient.kt
+++ android/app/src/main/java/com/krishna/meetingassistant/WebSocketClient.kt
@@ -127,7 +127,7 @@ class WebSocketClient {
             Log.w(TAG, "Not connected, dropping audio chunk")
             return
         }
-        webSocket?.send(RequestBody.create(null, audioChunk))
+        webSocket?.send(okio.ByteString.of(audioChunk, 0, audioChunk.size))
     }
 
     fun disconnect() {
